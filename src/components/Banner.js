46dcd846618a76f330add92b019edd8780f9cc02
Shubhank Srivastava <shubhank.srivastava@wynk.in>
2019-07-18 18:23:59 +0530

    bottom text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,21 +10,19 @@ class Banner extends Component {
         const { bottomText, ideas } = this.props;
 
         return <div id="banner">
-            <div className="banner-container">
-                <div className="top">
-                    {/*ideas.map(idea => {
-                        return <div>
-                            <span className="span-text">{idea.creator}</span>
-                            <br />
-                            <span className="span-text">{idea.title}</span>
-                            <br />
-                            <span className="span-text">{idea.description}</span>
-                        </div>
-                    })*/}
-                </div>
-                <div className="bottom">
-                    {bottomText}
-                </div>
+            <div className="top">
+                {/*ideas.map(idea => {
+                    return <div>
+                        <span className="span-text">{idea.creator}</span>
+                        <br />
+                        <span className="span-text">{idea.title}</span>
+                        <br />
+                        <span className="span-text">{idea.description}</span>
+                    </div>
+                })*/}
+            </div>
+            <div className="bottom">
+                {bottomText}
             </div>
         </div>
     }
