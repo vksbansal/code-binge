0364fd83f62019c921a14dfb73ade95d2c3883be
vikas bansal <vikas.bansal@wynk.in>
2019-07-18 17:46:25 +0530

    Updates

--- precache-manifest.f00d9074d07e9f1b5d55ce010b9221d8.js
+++ precache-manifest.f00d9074d07e9f1b5d55ce010b9221d8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "70768e2497ac9a137fce0f9f24ffeef8",
+    "revision": "ef749335057ee3675b641dd9ffd5882d",
     "url": "/code-binge/index.html"
   },
   {
-    "revision": "f493b4cac52e3e41a4ea",
-    "url": "/code-binge/static/css/main.a9492fa8.chunk.css"
+    "revision": "001cf8f032e814910cbc",
+    "url": "/code-binge/static/css/main.bc37902d.chunk.css"
   },
   {
     "revision": "eae02f7f28e5f7734541",
     "url": "/code-binge/static/js/2.ca771bea.chunk.js"
   },
   {
-    "revision": "f493b4cac52e3e41a4ea",
-    "url": "/code-binge/static/js/main.d774946c.chunk.js"
+    "revision": "001cf8f032e814910cbc",
+    "url": "/code-binge/static/js/main.4ee84408.chunk.js"
   },
   {
     "revision": "231db94be5fef140f6e1",
