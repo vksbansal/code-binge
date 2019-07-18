e05aae60217d8982600d8e9e46021bff8b3a3fa1
vikas bansal <vikas.bansal@wynk.in>
2019-07-18 17:37:48 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Banner from './components/Banner';
 import Countdown from './components/Countdown';
 import Footline from './components/Footline';
+import Logo from './components/Logo';
 
 const targetDate = new Date('2019-07-24 23:59:59');
 const prefix = "Idea Box Closes In:"
 const FootlineText = 'Exciting Prizes to be won for the Most Popular Idea'
+const logoSrc = "/logo.jpg"
 
 function App() {
   return (
     <div className="App">
+      <Logo url={logoSrc}></Logo>
       <div id="top"></div>
       <div id="bottom"> </div>
       <Banner></Banner>
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import '../styles/Logo.css'
 
 class Logo extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return <div>
-            
-        </div>;
+    render() {
+        const { url } = this.props;
+        return <div className="logo">
+            <img alt="logo" src={url}></img>
+        </div>
     }
 }
- 
+
 export default Logo;
