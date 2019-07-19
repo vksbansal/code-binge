ea013a28e673a4f336041cb9346559d23f607599
Shubhank Srivastava <shubhank.srivastava@wynk.in>
2019-07-19 16:39:54 +0530

    Time to hackathon launch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import moment from 'moment'
 
 const targetDate = moment("2019-07-24 23:59:59", "YYYY-MM-DD hh:mm:ss").toDate()
-const prefix = "Time to hackathon launch:"
+const prefix = "Time to Hackathon launch:"
 const FootlineText = 'Exciting Prizes to be won for the Most Popular Idea'
 const bannerBottomText = "SUBMIT YOUR IDEA BY 24TH JULY 2019"
 class App extends Component {
