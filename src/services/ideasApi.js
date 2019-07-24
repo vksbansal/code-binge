bbb8649ce821687e6707bf734799a7e62454d8e8
Shubhank Srivastava <shubhank.srivastava@wynk.in>
2019-07-24 16:11:52 +0530

    Updated json

--- src/services/ideasApi.js
+++ src/services/ideasApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const fetchIdeas = () => {
-    return axios.get(window.location.href + '/data.json')
+    return axios.get("https://code-binge.s3.ap-south-1.amazonaws.com/tricide-dump/ideas.json")
 }
